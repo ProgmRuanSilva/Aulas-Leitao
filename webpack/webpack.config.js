263330ab7120d6b57a71d3e5d1160dac37f83bfd
Ruan Silva <progmruansilva@gmail.com>
2022-10-02 03:23:43 -0300

    add jquery folder

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,4 +1,5 @@
 const webpack = require('webpack')
+const MiniCSS = require('mini-css-extract-plugin')
 
 //frist select mode
 module.exports = {
@@ -8,11 +9,17 @@ module.exports = {
         filename: 'principal.js',
         path: __dirname + '/public'
     },
+    plugins: [
+        new MiniCSS({
+            filename: "estilo.css"
+        })
+    ],
     module: {
         rules: [{
             test: /\.css$/, //ler aquivo css
             use: [
-                'style-loader', //injeta css dentro da dom <style>
+                MiniCSS.loader,
+                //'style-loader', //injeta css dentro da dom <style>
                 'css-loader' //interpretador dos @import, url()...
             ]
         }]
