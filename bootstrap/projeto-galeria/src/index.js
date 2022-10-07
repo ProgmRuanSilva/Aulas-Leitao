a23ebe7e067919ea07f805665d37d253d6587a43
Ruan Silva <progmruansilva@gmail.com>
2022-10-06 22:18:06 -0300

    imports

--- bootstrap/projeto-galeria/src/index.js
+++ bootstrap/projeto-galeria/src/index.js
@@ -1,2 +1,9 @@
 //scss arquives
 import './scss/index.scss'
+
+//Dependencies
+import 'jquery'
+import 'bootstrap' //sem loader webpack entende que serã um js
+
+//My arquives
+import './js/core/includes'
