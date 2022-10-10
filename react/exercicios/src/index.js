0eeb7f698bf6c908e99edeb617e824c5fc618b0a
Ruan Silva <progmruansilva@gmail.com>
2022-10-10 20:59:25 -0300

    FatherSon

--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom'
 import Multi, { BoaNoite } from './components/Multiplos'
 
 import Saudacao from './components/Saudacao'
+import Pai from './components/Pai'
+
 
 //ele transpila em jsx 
 ReactDOM.render(
@@ -11,6 +13,8 @@ ReactDOM.render(
         <Saudacao tipo="Bom dia" nome="Ruan"/>
         {/* <Multi.BoaTarde nome="Bia"/>
         <BoaNoite nome="Ana"/> */}
+        
+        <Pai nome="Paulo" sobrenome="Silva"/>
     </div>
     , document.getElementById('root'))
 //os componetes criados por vc devem começar com letra maiuscula
