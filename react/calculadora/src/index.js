cb6a5caa9ab488d34a0d90ad0241b8ae29f0e6d0
Ruan Silva <progmruansilva@gmail.com>
2022-10-10 22:20:51 -0300

    frist settings

--- react/calculadora/src/index.js
+++ react/calculadora/src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Calculator from './main/Calculator';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div>
+    <h1>Calculadora</h1>
+    <Calculator/>
+  </div>
 );
 
 // If you want to start measuring performance in your app, pass a function
