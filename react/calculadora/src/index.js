625f6480e10dbfe4e32a1756dfd43f49e26963b3
Ruan Silva <progmruansilva@gmail.com>
2022-10-10 23:24:20 -0300

    buttons 2

--- react/calculadora/src/index.js
+++ react/calculadora/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Calculator from './main/Calculator';
 import reportWebVitals from './reportWebVitals';
-import Button from './components/Button';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- react/calculadora/src/main/Calculator.jsx
+++ react/calculadora/src/main/Calculator.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import Button from "../components/Button.jsx";
 import './Calculator.css'
+import Display from '../components/Display'
 
 export default class Calculator extends Component {
     render() {
         return (
             <div className="calculator">
+                <Display value={100} />
                 <Button label="AC"/>
                 <Button label="/"/>
                 <Button label="7"/>
