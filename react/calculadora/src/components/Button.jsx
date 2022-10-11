3207e19a4b284ddeb0000754fcf430a373663c4c
Ruan Silva <progmruansilva@gmail.com>
2022-10-10 23:50:35 -0300

    style finished

--- react/calculadora/src/components/Button.jsx
+++ react/calculadora/src/components/Button.jsx
@@ -2,4 +2,14 @@ import React from "react";
 import './Button.css'
 
 export default props => 
-    <button className="button">{props.label}</button>
+    <button 
+    onClick={e => props.click(props.label)}
+    className={`
+        button
+        ${props.operation ? 'operation' : ''} 
+        ${props.double ? 'double' : ''} 
+        ${props.triple ? 'triple' : ''} 
+        `}>
+            {props.label}
+    </button>
+// com template strings tem que interpolar
