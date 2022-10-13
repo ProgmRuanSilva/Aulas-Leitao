1bd6a637601cd4b726f313a74ed0256dc51f480d
Ruan Silva <progmruansilva@gmail.com>
2022-10-13 13:28:44 -0300

    components and structure

---
+++ react/crud/frontend/src/main/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css"
+
+import Logo from '../components/templates/Logo'
+import Nav from '../components/templates/Nav'
+import Main from '../components/templates/Main'
+import Footer from "../components/templates/Footer"
+
+export default props => 
+    <div className="app">
+        <Logo/>
+        <Nav/>
+        <Main/>
+        <Footer/>
+    </div>
