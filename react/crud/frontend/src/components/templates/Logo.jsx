d404c61e4113a27f8989eccc4731a7f3081b0640
Ruan Silva <progmruansilva@gmail.com>
2022-10-14 14:05:05 -0300

    logo and header

--- react/crud/frontend/src/components/templates/Logo.jsx
+++ react/crud/frontend/src/components/templates/Logo.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import './Logo.css'
+import logo from '../../assets/images/logo.png'
 
 export default props => 
     <aside className="logo">
-        Logo
+        <a href="/" className="logo">
+            <img src={logo} alt="logo" />
+        </a>
     </aside>
--- react/crud/frontend/src/main/App.jsx
+++ react/crud/frontend/src/main/App.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./App.css"
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
 
 import Logo from '../components/templates/Logo'
 import Nav from '../components/templates/Nav'
@@ -10,6 +12,7 @@ export default props =>
     <div className="app">
         <Logo/>
         <Nav/>
-        <Main/>
+        <Main icon="home" title="Inicio"
+            subtitle="Segundo Projeto do Capitulo de React."/>
         <Footer/>
     </div>
