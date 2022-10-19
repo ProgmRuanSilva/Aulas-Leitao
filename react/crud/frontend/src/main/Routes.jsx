470f801e7797b79019af8a359184c2a701f16ecd
Ruan Silva <progmruansilva@gmail.com>
2022-10-19 10:52:36 -0300

    routes resolved

--- react/crud/frontend/src/main/Routes.jsx
+++ react/crud/frontend/src/main/Routes.jsx
@@ -5,8 +5,8 @@ import UserCrud from "../components/user/UserCrud";
 
 export default props => (
     <Routes>
-        <Route exact path="/" element={<Home />}/>
-        <Route path="/users" element={<UserCrud />} />
-        <Route path="*" element={<Home />} />
-    </ Routes>
+        <Route exact path='/' element={<Home />}/>
+        <Route path='/users' element={<UserCrud />} />
+        <Route path='*' element={<Home />} />
+    </Routes>
 );
