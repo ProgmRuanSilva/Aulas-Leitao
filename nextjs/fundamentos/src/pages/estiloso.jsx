8a03f5284c4b8ddd22582c4d63d064a556772225
Ruan Silva <progmruansilva@gmail.com>
2022-10-20 13:30:56 -0300

    new Layout

--- nextjs/fundamentos/src/pages/estiloso.jsx
+++ nextjs/fundamentos/src/pages/estiloso.jsx
@@ -1,8 +1,12 @@
 import styles from '../styles/Estiloso.module.css'
+import Link from 'next/link'
 
 export default function Estiloso() {
     return (
         <div className={styles}>
+            <Link href="/">
+                Voltar
+            </Link>
             <h1>Estilo Usando Módulos</h1>
         </div>
     )
