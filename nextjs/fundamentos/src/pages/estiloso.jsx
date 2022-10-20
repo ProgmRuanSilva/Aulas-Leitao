7ce35eed697412ae2eacc95644579d01438553e6
Ruan Silva <progmruansilva@gmail.com>
2022-10-20 17:10:39 -0300

    adding header

--- nextjs/fundamentos/src/pages/estiloso.jsx
+++ nextjs/fundamentos/src/pages/estiloso.jsx
@@ -1,12 +1,10 @@
 import styles from '../styles/Estiloso.module.css'
-import Link from 'next/link'
 import Layout from '../components/Layout'
 
 export default function Estiloso() {
     return (
         <Layout>
             <div className={styles}>
-                <Link href="/">Voltar</Link>
                 <h1>Estilo Usando Módulos</h1>
             </div>
         </Layout>
