ccb6ff3e71f559641234b6b60bf1bcde6a47ff76
Ruan <progmruansilva@gmail.com>
2022-10-20 23:53:49 -0300

    Navegacador

---
+++ nextjs/fundamentos/src/components/Navegador.jsx
@@ -0,0 +1,16 @@
+import Link from "next/link"
+import styles from '../styles/Navegador.module.css'
+
+export default function Navegador(props) {
+    return (
+        <div>
+            <Link href={props.destino}>
+                <div className={styles.navegador} style={{
+                    backgroundColor: props.cor ?? 'dodgerblue'
+                }}>
+                    {props.texto}
+                </div>
+            </Link>
+        </div>
+    )
+}
--- nextjs/fundamentos/src/pages/index.jsx
+++ nextjs/fundamentos/src/pages/index.jsx
@@ -3,13 +3,20 @@
 //dentro de pages em minusculo
 //severside redenring
 import Link from "next/link"
+import Navegador from "../components/Navegador"
 
 export default function Inicio() {
     return (
-        <div>
-            <Link href="/estiloso">
-                Estiloso
-            </Link>
+        <div style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            flexWrap: 'wrap',
+            height: '100vh'
+        }} >
+            <Navegador texto="Estiloso"destino="/estiloso"/>
+            <Navegador texto="Exemplo" destino="/exemplo" cor="#9400d3"/>
+            <Navegador texto="Jsx" destino="/jsx" cor="crimson"/>
         </div>
     )
 }
