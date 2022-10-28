b1394c696d97d267747c8fcc34b5210964e1fb1c
Ruan Silva <progmruansilva@gmail.com>
2022-10-28 08:16:13 -0300

    vue

---
+++ react/mobile/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Lucas 9:23</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontSize: '600px'
+  },
+});
