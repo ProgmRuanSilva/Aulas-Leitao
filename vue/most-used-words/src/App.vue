09d4b3b6036d6863fa45531baa53baecc1ae40e5
Ruan Silva <progmruansilva@gmail.com>
2022-11-08 16:36:29 -0300

    pill component with error

--- vue/most-used-words/src/App.vue
+++ vue/most-used-words/src/App.vue
@@ -9,18 +9,19 @@
     </v-app-bar>
 
     <v-main>
-
+      <Home />
     </v-main>
   </v-app>
 </template>
 
 <script>
+import Home from './components/Home'
 
 export default {
   name: 'App',
 
   components: {
-
+    Home
   },
 
   data: () => ({
--- vue/most-used-words/src/components/Home.vue
+++ vue/most-used-words/src/components/Home.vue
@@ -1,10 +1,26 @@
 <template>
-  
+    <v-container fluid>
+        <div class="pills">
+            <Pill-Item v-for="word in groupedWords" :key="word.name"
+              name="teste" amount="10"/>
+        </div>
+    </v-container>
 </template>
 
 <script>
-export default {
+import PillItem from './PillItem';
 
+export default {
+    components: {PillItem},
+    data: function() {
+        return {
+            groupedWords: [
+                { name: 'you', amout: 900},
+                { name: 'he', amout: 853},
+                { name: 'i', amout: 1234},
+            ]
+        }
+    }
 }
 </script>
 
