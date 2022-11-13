6222e9b873c5d7cd37efafeb18bdbba862099f3f
Ruan Silva <progmruansilva@gmail.com>
2022-11-12 23:22:04 -0300

    creating directive

--- angular/crud/frontend/src/app/app.module.ts
+++ angular/crud/frontend/src/app/app.module.ts
@@ -16,6 +16,7 @@ import { HomeComponent } from './views/home/home.component';
 
 import { MatCardModule } from '@angular/material/card';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+import { RedDirective } from './directives/red.directive';
 
 
 @NgModule({
@@ -25,7 +26,8 @@ import { ProductCrudComponent } from './views/product-crud/product-crud.componen
     FooterComponent,
     NavComponent,
     HomeComponent,
-    ProductCrudComponent
+    ProductCrudComponent,
+    RedDirective
   ],
   imports: [
     BrowserModule,
