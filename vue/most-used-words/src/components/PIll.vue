9eb712da8f7acc9ee5a95ccbacf1a52afe209e27
Ruan Silva <progmruansilva@gmail.com>
2022-11-12 23:08:02 -0300

    pill on vue

---
+++ vue/most-used-words/src/components/PIll.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <span>{{Nome}}</span>
+        <span>{{Quantidade}}</span>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['name', 'amout']
+}
+</script>
+
+<style>
+
+</style>
