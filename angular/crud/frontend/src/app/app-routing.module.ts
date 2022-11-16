f2bc91536bb6f0a40ef3f3a208c984af44c81e86
Ruan Silva <progmruansilva@gmail.com>
2022-11-16 00:28:32 -0300

    product-create add

--- angular/crud/frontend/src/app/app-routing.module.ts
+++ angular/crud/frontend/src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from'./views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,9 @@ const routes: Routes = [
 },{
   path: "products",
   component: ProductCrudComponent,
+},{
+  path: "products/create",
+  component: ProductCreateComponent
 }
 ];
 
--- angular/crud/frontend/src/app/app.module.ts
+++ angular/crud/frontend/src/app/app.module.ts
@@ -18,6 +18,7 @@ import { MatCardModule } from '@angular/material/card';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 import { RedDirective } from './directives/red.directive';
 import { ForDirective } from './directives/for.directive';
+import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 
 
 @NgModule({
@@ -29,7 +30,8 @@ import { ForDirective } from './directives/for.directive';
     HomeComponent,
     ProductCrudComponent,
     RedDirective,
-    ForDirective
+    ForDirective,
+    ProductCreateComponent,
   ],
   imports: [
     BrowserModule,
