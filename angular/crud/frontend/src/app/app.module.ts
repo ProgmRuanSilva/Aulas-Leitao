0b80fbe9ac3d3ceb553d28b5950dc32b381aa798
Ruan Silva <progmruansilva@gmail.com>
2022-11-17 14:52:23 -0300

    product-read2

--- angular/crud/frontend/src/app/app.module.ts
+++ angular/crud/frontend/src/app/app.module.ts
@@ -29,6 +29,10 @@ import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule }  from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { ProductReadComponent } from './components/product/product-read/product-read.component';
+import { ProductRead2Component } from './components/product/product-read2/product-read2.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
 
 
 @NgModule({
@@ -43,6 +47,7 @@ import { ProductReadComponent } from './components/product/product-read/product-
     ForDirective,
     ProductCreateComponent,
     ProductReadComponent,
+    ProductRead2Component,
   ],
   imports: [
     BrowserModule,
@@ -57,7 +62,10 @@ import { ProductReadComponent } from './components/product/product-read/product-
     HttpClientModule,
     MatFormFieldModule,
     MatInputModule,
-    FormsModule
+    FormsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
