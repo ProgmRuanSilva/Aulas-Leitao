bc43b193bcadbf1f1b6cb92fa9a73898c22d7e09
Ruan Silva <progmruansilva@gmail.com>
2022-11-28 10:01:30 -0300

    done iserir Estados

--- mysql/criarTabela.sql
+++ mysql/criarTabela.sql
@@ -4,8 +4,8 @@ create table estados (
     nome VARCHAR(45) NOT NULL,
     sigla VARCHAR(2) NOT NULL,
     regiao ENUM('Norte', 'Nordeste', 'Centro-Oeste', 'Sudeste', 'Sul') NOT NULL,
-    populacao DECIMAL(5, 2) NOT NULL,
+    populacao DECIMAL(5,2) NOT NULL,
     PRIMARY KEY (id),
     UNIQUE KEY (nome),
     UNIQUE KEY (sigla)
-)
+);
